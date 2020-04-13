f770dc8830a632c46e572d2a2a807a1b3ae44230
Michał Kopacz <kopaczmichal093@gmail.com>
2020-04-13 14:40:16 +0200

    Add 'active' class for targeted article

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
  */
 
 const titleClickHandler =  function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('link was clicked');
 
@@ -26,14 +27,23 @@ const titleClickHandler =  function (event) {
 
     /* [DONE] get 'href' attribute from the clicked link */
 
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
     console.log('clickedElement: ', clickedElement);
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    /* [DONE] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active');
 
-    /* find the correct article using the selector (value of 'href' attribute) */
-    /* add class 'active' to the correct article */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    /* [IN PROGRESS] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
 
 }
 
